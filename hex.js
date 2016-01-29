9620eabd24f4646f5efe613d34a3208de48b9fa3
Martin Grabmueller <martin@grabmueller.de>
2016-01-29 22:03:57 +0100

    More hex fun.

--- hex.js
+++ hex.js
@@ -5,53 +5,74 @@ var grid =
      [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
      [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
      [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
-     [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10],
      [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]];
 
 function render(canvas) {
     var ctx = canvas.getContext('2d');
-    var width = canvas.width,
-        height = canvas.height;
+
+    var hexSize = 32;
+    var a_size = hexSize / 2.0,
+        b_size = hexSize * Math.sqrt(3) / 2.0,
+        height = hexSize * 2.0,
+        width = b_size * 2.0;
 
     ctx.save();
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     ctx.lineWidth = 1;
-    ctx.clearRect(0, 0, width, height);
-    ctx.strokeRect(1, 1, width-2, height-2);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeRect(1, 1, canvas.width-2, canvas.height-2);
     ctx.translate(offsetX, offsetY);
 
     var pointy = true
-    var hexSize = 20;
+
+    var mouseRow = Math.floor((mouseY - offsetY) / (height - a_size)),
+	mouseRowRest = (mouseY - offsetY) - (mouseRow * (height - a_size));
+    var mouseCol = Math.floor((mouseX - offsetX - mouseRow * b_size) / width);
+    var mouseColRest = Math.floor(mouseX - offsetX - mouseRow * b_size) - (mouseCol * width);
+    if (mouseRowRest < a_size) {
+	if (mouseColRest < hexSize*Math.abs(0.5-mouseRow/width)) {
+	    mouseRow--;
+	} else if (mouseColRest>hexSize *Math.abs(0.5-mouseRow/width)) {
+	    mouseRow--;
+	    mouseCol++;
+	}
+//	console.log(mouseRowRest, mouseColRest);
+    }
     var x, y;
+    ctx.strokeStyle = 'lightgray';
+    for (y in grid) {
+        var row = grid[y];
+        for (x in row) {
+	    ctx.strokeRect(x * width + y * b_size,
+			   y * (height - a_size),
+			   width,
+			   height - a_size);
+
+	}
+    }
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokeStyle = 'black';
     for (y in grid) {
         var row = grid[y];
         for (x in row) {
 
-            var a_size = hexSize / 2.0,
-                b_size = hexSize * Math.sqrt(3) / 2.0,
-                height = hexSize * 2.0,
-                width = b_size * 2.0;
+	    if (x == mouseCol && y == mouseRow) {
+		var cx = b_size + x * width + y * b_size,
+                    cy = hexSize + 3 * y * a_size;
 
-            if (pointy) {
+		ctx.beginPath()
+		ctx.moveTo(cx - b_size, cy - a_size);
+		ctx.lineTo(cx, cy - hexSize);
+		ctx.lineTo(cx + b_size, cy - a_size);
+		ctx.lineTo(cx + b_size, cy + a_size);
+		ctx.lineTo(cx, cy + hexSize);
+		ctx.lineTo(cx - b_size, cy + a_size);
+		ctx.lineTo(cx - b_size, cy - a_size);
+		ctx.fill();
+	    }
             var cx = b_size + x * width + y * b_size,
                 cy = hexSize + 3 * y * a_size;
-            // var cx = hexSize + 3 * x * a_size + y * hexSize,
-            //     cy = b_size + y * height;
             ctx.beginPath()
             ctx.moveTo(cx - b_size, cy - a_size);
             ctx.lineTo(cx, cy - hexSize);
@@ -62,43 +83,38 @@ function render(canvas) {
             ctx.lineTo(cx - b_size, cy - a_size);
             ctx.stroke();
 //            ctx.strokeText(x + ',' + y, cx, cy);
-            } else {
-            var cx = hexSize + 3 * x * a_size,
-                cy = b_size + y * height + x * b_size;
-            ctx.beginPath()
-            ctx.moveTo(cx - hexSize, cy);
-            ctx.lineTo(cx - a_size, cy - b_size);
-            ctx.lineTo(cx + a_size, cy - b_size);
-            ctx.lineTo(cx + hexSize, cy);
-            ctx.lineTo(cx + a_size, cy + b_size);
-            ctx.lineTo(cx - a_size, cy + b_size);
-            ctx.lineTo(cx - hexSize, cy);
-            ctx.stroke();
-                ctx.fillText(x + ',' + y, cx, cy);
-            }
         }
     }
     ctx.restore();
 }
 
+var mouseX = 0,
+    mouseY = 0;
 var angle = 0;
-var offsetX = 0;
-var offsetY = 0;
+var offsetX = 10;
+var offsetY = 10;
 
 function update() {
-    angle += 0.05;
-    offsetX = Math.cos(angle) * 50;
-    offsetY = Math.sin(angle) * 50-50;
+    angle += 0.01;
+    offsetX = Math.cos(angle) * 50 + 50;
+    offsetY = Math.sin(angle) * 50 + 50;
 }
 
 function loop(canvas) {
     requestAnimationFrame(function() {loop(canvas)});
-    update();
+//    update();
     render(canvas);
 }
 
 function start(canvasId) {
     var canvas = document.getElementById(canvasId);
 
+    canvas.addEventListener("mousemove",
+			      function(e) {
+				  var x = e.clientX - 20,
+				      y = e.clientY - 20;
+				  mouseX = x;
+				  mouseY = y;
+			      });
     requestAnimationFrame(function() {loop(canvas)});
 }
